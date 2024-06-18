8ecb207165dcafe51317c9ab13410bc757a1aba5
정현석 <155929257+hyunseokjung00@users.noreply.github.com>
2024-06-18 13:37:59 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ AwsRegion="${INPUT_AWS_REGION}"
 CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com.cn/v1/repos/${RepositoryName}"
 
 git config --global --add safe.directory /github/workspace
+
 #git config --global credential.'https://git-codecommit.*.amazonaws.com.cn'.helper '!aws codecommit credential-helper $@'
-#git config --global credential.UseHttpPath true
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
 git remote add sync ${CodeCommitUrl}
 git push sync --mirror
 .
